8e407da5a3158d0e4c6ac86b67ef15ef99acdf73
JoeKronhoj <Josefine@Kronhoj.dk>
2019-12-08 13:53:25 +0100

    Alle elementer er blevet 2D

--- Assets/Scripts/Bird.cs
+++ Assets/Scripts/Bird.cs
@@ -9,11 +9,12 @@ public class Bird : MonoBehaviour
     private void OnTriggerEnter(Collider objectHits)
     {
         Debug.Log("Trigger");
-        Instantiate(note);
+        //Instantiate(note, new Vector3(8f,0.3f,0.3f),transform.rotation);
 
         if (objectHits.gameObject.tag == "Player")
         {
-            GetComponent<Rigidbody>().AddForce(new Vector3(500, 500, 3000));
+            GetComponent<Rigidbody>().AddForce(new Vector3(500, 500, 500));
+            Instantiate(note, new Vector3(8f, 0f, 0f), transform.rotation);
         }
     }
 }
