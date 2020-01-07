cc3893086688416fe288c3d3adc5807df84d9e2f
JoeKronhoj <Josefine@Kronhoj.dk>
2020-01-07 21:42:08 +0100

    Pickup notes + Bird note

---
+++ Assets/Scripts/GUI_Button.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GUI_Button : MonoBehaviour
+{
+
+    public GameObject sunglasses;
+
+    private GameObject guiOBject;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void myClick()
+    {
+        if (guiOBject)
+        {
+            Destroy(guiOBject);
+        }
+        else
+        {
+            GameObject mainCamera;
+            mainCamera = GameObject.FindGameObjectsWithTag("MainCamera")[0];
+
+            guiOBject = Instantiate(sunglasses, mainCamera.transform);
+        }
+    }
+}
