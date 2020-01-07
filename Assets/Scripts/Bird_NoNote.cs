af4bcd3cdc7f11c154a1d5a36da593497c3dcd72
JoeKronhoj <Josefine@Kronhoj.dk>
2020-01-07 22:27:16 +0100

    Notes working + more birds

---
+++ Assets/Scripts/Bird_NoNote.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bird_NoNote : MonoBehaviour
+{
+    
+
+    private void OnTriggerEnter(Collider objectHits)
+    {
+        Debug.Log("Trigger");
+        //Instantiate(note, new Vector3(8f,0.3f,0.3f),transform.rotation);
+
+        if (objectHits.gameObject.tag == "Player")
+        {
+            gameObject.GetComponent<Animator>().Play("2_Bird_FlyOff");
+            GetComponent<Rigidbody>().AddForce(new Vector3(500, 500, 500));
+          
+        }
+    }
+}
