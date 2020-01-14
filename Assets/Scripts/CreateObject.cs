6be285f1cb2fa41a6910aa7fbd61f7b5330af1ed
Lamselord <57187151+Lamselord@users.noreply.github.com>
2020-01-14 20:56:42 +0100

    adding sprites and createObejct ontriggerEnter.

---
+++ Assets/Scripts/CreateObject.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateObject : MonoBehaviour
+{
+    public Transform Spawnpoint;
+    public GameObject Prefab;
+
+    void OnTriggerEnter(UnityEngine.Collider other)
+    {
+        Instantiate(Prefab, Spawnpoint.position, Spawnpoint.rotation);  
+    }
+
+
+}
