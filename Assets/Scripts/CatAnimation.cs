f977eafd926315090d664fe65f9dba3454301876
JoeKronhoj <Josefine@Kronhoj.dk>
2020-01-15 14:06:15 +0100

    Sky + Funny Cat Attack

---
+++ Assets/Scripts/CatAnimation.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CatAnimation : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider objectHits)
+    {
+        Debug.Log("AnimationTrigger");
+        //Instantiate(note, new Vector3(8f,0.3f,0.3f),transform.rotation);
+
+        if (objectHits.gameObject.tag == "Bird")
+        {
+            gameObject.GetComponent<Animator>().Play("Cat_Attack");
+        }
+    }
+}
--- Assets/Scripts/KittyKat.cs
+++ Assets/Scripts/KittyKat.cs
@@ -6,23 +6,32 @@ using UnityEngine.AI;
 public class KittyKat : MonoBehaviour
 {
     NavMeshAgent agent;
-    //public Inventory inventory; 
-   
+    //public Inventory inventory;
+
+
+
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
+
     }
 
     void Update()
     {
         if (Input.GetMouseButtonDown(0))
         {
+
             RaycastHit hit;
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             if (Physics.Raycast(ray, out hit, Mathf.Infinity))
             {
                 agent.SetDestination(hit.point);
+                
+
             }
         }
     }
-} 
+
+   
+
+}
