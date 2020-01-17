0a0be40074104c6c967e597c49f50e489326ac12
Lamselord <57187151+Lamselord@users.noreply.github.com>
2020-01-17 18:27:22 +0100

    fade out to next scene

---
+++ Assets/Animation/LevelChanger.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+
+public class LevelChanger : MonoBehaviour
+{
+    public Animator animator;
+
+    private int LevelToLoad;
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(1))
+        {
+            FadeToLevel(1);
+        }
+
+    }
+
+    public void FadeToLevel (int levelIndex)
+    {
+        LevelToLoad = levelIndex;
+        animator.SetTrigger("FadeOut");
+    }
+
+    public void OnFadeComplete()
+    {
+        SceneManager.LoadScene(LevelToLoad);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+}
