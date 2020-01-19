ed085ab5cba7e452502d14eb5d5737aae2081d90
JoeKronhoj <Josefine@Kronhoj.dk>
2020-01-19 19:50:22 +0100

    BirdFlipHalf

---
+++ Assets/Scripts/BirdFlip.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BirdFlip : MonoBehaviour
+{
+    SpriteRenderer spriteRenderer;
+    private float x_trans;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        x_trans = transform.position.x;
+        Debug.Log(x_trans);
+     
+        spriteRenderer.flipX = true;
+        
+    }
+}
--- Assets/Scripts/BirdPatrol.cs
+++ Assets/Scripts/BirdPatrol.cs
@@ -11,21 +11,30 @@ public class BirdPatrol : MonoBehaviour
     public Transform[] moveSpots;
     private int randomSpot;
 
+
     private void Start()
     {
+        
         waitTime = startWaitTime;
         randomSpot = Random.Range(0, moveSpots.Length);
+     
+        
+        
+
     }
     private void Update()
     {
         transform.position = Vector2.MoveTowards(transform.position, moveSpots[randomSpot].position, speed * Time.deltaTime);
-
+    
+   
         if (Vector2.Distance(transform.position, moveSpots[randomSpot].position)<0.2f)
         {
+
             if(waitTime <= 0)
             {
                 randomSpot = Random.Range(0, moveSpots.Length);
-                waitTime = startWaitTime;   
+                waitTime = startWaitTime;
+                
             } else
             {
                 waitTime -= Time.deltaTime;
